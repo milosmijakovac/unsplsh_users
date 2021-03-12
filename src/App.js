44751b02ade9ccb712fd5e2c703f7d566bf3c239
milos mijakovac <milosmijakovac@gmail.com>
2021-03-12 17:35:35 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import Header from "./components/Header";
+import axios from 'axios'
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+
+  useEffect(() => {
+    const apiRoot = "https://api.unsplash.com";
+    const accessKey = process.env.REACT_APP_ACCESS_KEY;
+
+    // axios.get(`${apiRoot}/search/users?client_id=${accessKey}&page=1`).then(res => console.log(res.data))
+
+    axios.get(`${apiRoot}/search/users?page=1&per_page=20&query=${searchTerm}`, {
+      headers: {
+       Authorization: `Client-ID ${accessKey}`
+      }
+    }).then(res => setUsers(res.data))
+  }, [searchTerm])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pera">
+      <Header /> 
+      <input 
+      type="text"
+      value={searchTerm}
+      onChange={e => setSearchTerm(e.target.value)}
+      />
+      <h1>cao cao</h1>
+      {
+      users.results && users.results.filter(user =>{ user.name.toLowerCase().includes(searchTerm.toLowerCase()) || user.last_name.toLowerCase().includes(searchTerm.toLowerCase())}
+      ).map(user => {
+       return (
+         <div>
+           <h1>{user.name} {user.last_name}</h1>
+         </div>
+       )
+      })}
+
+      
     </div>
   );
 }
